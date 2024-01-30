107fff89c8579e623ba646e96a6150c4daf40b90
jackf21 <jack.fenning04@gmail.com>
2024-01-30 12:32:41 +0000

    Update CorrectNumberGeneration.cs

--- 1stGame/Assets/Scripts/CorrectNumberGeneration.cs
+++ 1stGame/Assets/Scripts/CorrectNumberGeneration.cs
@@ -16,7 +16,7 @@ public class CorrectNumberGeneration : MonoBehaviour
 
     int[] textPositions = new int[4];
 
-    //Number = shape position in final pannel array
+    //Number to display = shape position in final pannel array
     //Position of number (in textPositions) = shape position in initial pannel array
 
     //For each element in initial pannel array find the matching one in final pannel array
